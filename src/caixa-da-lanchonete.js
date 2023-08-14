e0c028ad0b7ae4c6360ed3daf7a760126210a94f
yagosiq <yagosiqti@gmail.com>
2023-08-14 10:57:31 -0300

    Improving logic and adding progressive discounts

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,26 +1,71 @@
 class CaixaDaLanchonete {
 
     constructor() {
-        // Definindo os preços dos itens e combos aqui
         this.prices = {
           cafe: 2.5,
           chantily: 1.5,
-          combo1: 10,
+          suco: 6.2,
+          sanduiche: 6.5,
+          queijo: 2.0,
+          salgado: 7.25,
+          combo1: 9.5,
+          combo2: 7.5
         };
       }
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        // Verificando a forma de pagamento 
-    if (formaDePagamento !== 'debito' && formaDePagamento !== 'credito' && formaDePagamento !== 'dinheiro') {
-      return 'Forma de pagamento inválida!';
+      calcularValorDaCompra(formaDePagamento, itens) {
+        const formasDePagamentoValidas = ['debito', 'credito', 'dinheiro'];
+        const itemQuantities = {};
+    
+        if (!formasDePagamentoValidas.includes(formaDePagamento)) {
+          return 'Forma de pagamento inválida!';
+        }
+    
+        if (itens.length === 0) {
+          return 'Não há itens no carrinho de compra!';
+        }
+    
+        for (const item of itens) {
+          const [itemCode, quantity] = item.split(',');
+    
+          if (!this.prices[itemCode]) {
+            return 'Item inválido!';
+          }
+    
+          if (!itemQuantities[itemCode]) {
+            itemQuantities[itemCode] = 0;
+          }
+    
+          itemQuantities[itemCode] += parseInt(quantity);
+        }
+    
+        if (itemQuantities.chantily && !itemQuantities.cafe) {
+          return 'Item extra não pode ser pedido sem o principal';
+        }
+    
+        if (itemQuantities.cafe === 0) {
+          return 'Quantidade inválida!';
+        }
+    
+        let total = 0;
+        for (const itemCode in itemQuantities) {
+          if (this.prices[itemCode]) {
+            total += this.prices[itemCode] * itemQuantities[itemCode];
+          }
+        }
+    
+        if (formaDePagamento === 'dinheiro') {
+          total *= 0.95;  
+        } else if (formaDePagamento === 'credito') {
+          total *= 1.03; 
+        }
+    
+        const formattedTotal = total.toFixed(2).replace('.', ',');
+    
+        return `R$ ${formattedTotal}`;
+      }
     }
-
-    // Criando um objeto para armazenar a contagem de itens
-    const itemQuantities = {};
-
     
-  }
-}
-    
+   
 
 export { CaixaDaLanchonete };
