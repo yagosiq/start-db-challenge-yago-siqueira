fc26b4054988d5a1e543dcdb64b6de1dcf9bd3a2
yagosiq <yagosiqti@gmail.com>
2023-08-14 10:25:11 -0300

    Add cinstructor

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,5 +1,14 @@
 class CaixaDaLanchonete {
 
+    constructor() {
+        // Defina os preços dos itens e combos aqui
+        this.prices = {
+          cafe: 2.5,
+          chantily: 1.5,
+          combo1: 10,
+        };
+      }
+
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
     }
